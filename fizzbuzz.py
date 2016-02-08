755e2342e3e63699eb3b6380c827207779ed5017
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-08 14:26:03 -0500

    Updated from Brython Server: 2/8/2016 2:26:35 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -49,10 +49,10 @@ n = int( input("How many numbers shall we print? "))
 f = int( input("For multiples of what number shall we print 'Fizz'? "))
 b = int(input("For multiples of what number shall we print 'Buzz'? "))
 
-for x in range(1, n+1):
-    if x%f=0:
+for x in range(1, n+1)
+    if x%f=0
         x=Fizz
-    if x%b = 0:
+    if x%b = 0
         x=Buzz
     if x%b=0 and x%f=0
         x = FizzBuzz
